411217ea6a010142a9a8dfbc8822cf6e7e64109d
Viktoriia Doshchenko <doshchenko.victoria@gmail.com>
2022-07-20 11:51:12 +0200

    fix(mui): migrate date/time pickers

--- packages/editor-pro/src/editor/editor.tsx
+++ packages/editor-pro/src/editor/editor.tsx
@@ -5,8 +5,6 @@ import { Menu, Pane, majorScale } from 'evergreen-ui';
 import MenuItem, { MenuItemProps } from '@data-driven-forms/editor-core/menu-item';
 import CoreEditor from '@data-driven-forms/editor-core/editor';
 import TopNav from './top-nav';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
 import EditorContent from './editor-content';
 import PropertiesCard from './properties-card';
 import { Field, FormRenderer, ComponentMapper, Schema } from '@data-driven-forms/react-form-renderer';
@@ -27,54 +25,52 @@ export interface EditorProps {
 
 const Editor = ({ componentMapper, componentInitialProps, fields, initialSchema }: EditorProps) => {
 	return (
-		<LocalizationProvider dateAdapter={AdapterDateFns}>
-			<Pane
-				display="flex"
-				flexDirection="column"
-				height="100vh"
+		<Pane
+			display="flex"
+			flexDirection="column"
+			height="100vh"
+		>
+			<CoreEditor
+				DropCursorProps={{
+					CursorProps: {
+						className: 'drop-cursor'
+					}
+				}}
+				initialSchema={initialSchema}
 			>
-				<CoreEditor
-					DropCursorProps={{
-						CursorProps: {
-							className: 'drop-cursor'
-						}
-					}}
-					initialSchema={initialSchema}
-				>
-					<TopNav />
-					<Pane flex="1" width="100%" display="flex">
-						<Pane
-							display="flex"
-							position="sticky"
-							flexDirection="column"
-							overflowY="auto"
-							maxHeight="calc(100vh - 48px)"
-							paddingX={majorScale(1)}
-							top="48px"
-						>
-							<Menu>
-								<Menu.Group title="Components">
-									{Object.keys(componentMapper).map(key => <MenuItemWrapper key={key} component={key} componentInitialProps={componentInitialProps?.[key]}>
-										{key.replaceAll('-', ' ')}
-									</MenuItemWrapper>)}
-								</Menu.Group>
-							</Menu>
-						</Pane>
-						<Pane flex="1" display="flex" flexDirection="column">
-							<FormRenderer
-								schema={{ fields: [] }}
-								onSubmit={() => undefined}
-								componentMapper={{ ...componentMapper, 'sub-form': SubForm }}
-								FormTemplate={() => <ContainerWrapper isRoot Component={ComponentWrapper} />}
-							/>
-							<Code />
-						</Pane>
-						<PropertiesCard fields={fields} />
-						<EditorContent fields={fields} />
+				<TopNav />
+				<Pane flex="1" width="100%" display="flex">
+					<Pane
+						display="flex"
+						position="sticky"
+						flexDirection="column"
+						overflowY="auto"
+						maxHeight="calc(100vh - 48px)"
+						paddingX={majorScale(1)}
+						top="48px"
+					>
+						<Menu>
+							<Menu.Group title="Components">
+								{Object.keys(componentMapper).map(key => <MenuItemWrapper key={key} component={key} componentInitialProps={componentInitialProps?.[key]}>
+									{key.replaceAll('-', ' ')}
+								</MenuItemWrapper>)}
+							</Menu.Group>
+						</Menu>
 					</Pane>
-				</CoreEditor>
-			</Pane>
-		</LocalizationProvider>
+					<Pane flex="1" display="flex" flexDirection="column">
+						<FormRenderer
+							schema={{ fields: [] }}
+							onSubmit={() => undefined}
+							componentMapper={{ ...componentMapper, 'sub-form': SubForm }}
+							FormTemplate={() => <ContainerWrapper isRoot Component={ComponentWrapper} />}
+						/>
+						<Code />
+					</Pane>
+					<PropertiesCard fields={fields} />
+					<EditorContent fields={fields} />
+				</Pane>
+			</CoreEditor>
+		</Pane>
 	);
 };
 
